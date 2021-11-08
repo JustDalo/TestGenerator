82f08c6e041dccbb74b782b77198532c4f0466d5
JustDalo <shishlodan@mail.ru>
2021-11-08 16:29:27 +0300

    file structure

--- TestGeneratorLibrary/TestGenerator/IGenerator.cs
+++ TestGeneratorLibrary/TestGenerator/IGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
-using System.IO;
+using TestGeneratorLibrary.TestStructureInfo;
+
 
 namespace TestGeneratorLibrary.TestGenerator
 {
--- TestGeneratorLibrary/TestGenerator/TestGeneratorImpl/TestGenerator.cs
+++ TestGeneratorLibrary/TestGenerator/TestGeneratorImpl/TestGenerator.cs
@@ -1,24 +1,65 @@
+using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Text;
+
+using TestGeneratorLibrary.TestStructureInfo;
+
 
 namespace TestGeneratorLibrary.TestGenerator.TestGeneratorImpl
 {
-    public class TestGenerator
-    {
-        
-        public TestGenerator()
-        {
-            
-        }
-
+    public class TestGenerator : IGenerator
+    { 
         public Dictionary<string,string> Generate(FileInfo fileInfo)
         {
-            return null;
+            var fileCode = new Dictionary<string, string>();
+            foreach (var classDescription in fileInfo.Classes)
+            {
+                
+                fileCode.Add(classDescription.ClassName + "Test", GenerateTests(classDescription));
+            }
+            return fileCode;
+        }
+        
+        
+
+        private string GenerateTests(ClassInfo classDescription)
+        {
+            var sb = new StringBuilder();
+            foreach (var method in classDescription.Methods)
+            {
+                foreach (var methodParameter in method.Parameters)
+                {
+                    sb.Append(ArrangeBlock(methodParameter));
+                }    
+            }
+            return sb.ToString();
         }
 
-        private IEnumerable<string> GenerateTest()
+        private string LibraryUsing()
+        {
+            var sb = new StringBuilder();
+            sb.Append(
+                $@"using NUnit.Framework;
+                   using Moq;
+                    "
+            );
+            return sb.ToString();
+        }
+
+        private string ArrangeBlock(ParameterInfo parameterInfo)
+        {
+            var sb = new StringBuilder();
+            sb.Append(
+                $@"{parameterInfo.ParameterType} {parameterInfo.ParameterName} = 0;");
+            return sb.ToString();
+        }
+        
+        object GetDefaultValue(Type t)
         {
             
+            if (t.IsValueType)
+                return Activator.CreateInstance(t);
+
             return null;
         }
     }
