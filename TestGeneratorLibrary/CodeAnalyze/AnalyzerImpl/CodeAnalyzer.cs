80baa94258e09b8399850ea3ed99834274dee791
JustDalo <shishlodan@mail.ru>
2021-11-08 16:20:09 +0300

    code analyze

--- TestGeneratorLibrary/CodeAnalyze/AnalyzerImpl/CodeAnalyzer.cs
+++ TestGeneratorLibrary/CodeAnalyze/AnalyzerImpl/CodeAnalyzer.cs
@@ -1,30 +1,64 @@
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TestGeneratorLibrary.TestStructureInfo;
 using CompilationUnitSyntax = Microsoft.CodeAnalysis.CSharp.Syntax.CompilationUnitSyntax;
 
 namespace TestGeneratorLibrary.CodeAnalyze.AnalyzerImpl
 {
     public class CodeAnalyzer : IAnalyzer
     {
-        private List<string> CodeMethods;
-        public CodeAnalyzer()
-        {
-            CodeMethods = new List<string>();
-        }
         public FileInfo AnalyzeFile(string analyzableCode)
         {
             CompilationUnitSyntax root = CSharpSyntaxTree.ParseText(analyzableCode).GetCompilationUnitRoot();
-            var classes = new List<string>();
+            var classes = new List<ClassInfo>();
             foreach (var classDeclaration in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
             {
-                
+                classes.Add(GetClassMethods(classDeclaration));
             }
-            return null;
+            return new FileInfo(classes);
         }
+
+        private ClassInfo GetClassMethods(ClassDeclarationSyntax classDeclaration)
+        {
+            var methods = new List<MethodInfo>();
+            foreach (var methodDeclarationSyntax in classDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>()
+                .Where((methodDeclaration) => methodDeclaration.Modifiers.Any((modifier) 
+                => modifier.IsKind(SyntaxKind.PublicKeyword))))
+            {
+                methods.Add(GetMethodInfo(methodDeclarationSyntax));
+            }
+
+            var constructors = new List<ConstructorInfo>();
+            foreach (var constructorDeclarationSyntax in classDeclaration.DescendantNodes().OfType<ConstructorDeclarationSyntax>())
+            {   
+                constructors.Add(GetConstructorInfo(constructorDeclarationSyntax));
+            }
+            return new ClassInfo(methods, constructors, classDeclaration.Identifier.Text);
+        }
+
+        private MethodInfo GetMethodInfo(MethodDeclarationSyntax method)
+        {
+            var parameters = new List<ParameterInfo>();
+            foreach (var parameter in method.ParameterList.Parameters)
+            {
+                parameters.Add(new ParameterInfo(parameter.Type?.ToString(), parameter.Identifier.ValueText));
+            }
+            return new MethodInfo(parameters, method.Identifier.ValueText, method.ReturnType.ToString());
+        }
+
+        private ConstructorInfo GetConstructorInfo(ConstructorDeclarationSyntax constructor)
+        {
+            var parameters = new List<string>();
+            foreach (var parameter in constructor.ParameterList.Parameters)
+            {
+                parameters.Add(parameter.Identifier.Parent?.ToString());
+            }
+            return new ConstructorInfo(parameters, constructor.Identifier.Text);
+        }
+        
+        
     }
 }
