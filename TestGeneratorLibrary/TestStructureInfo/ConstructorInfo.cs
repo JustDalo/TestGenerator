7df8721a4d63a1e3f60751c68830ea3e17b03307
JustDalo <shishlodan@mail.ru>
2021-11-10 23:26:59 +0300

    constructor parameter changed

--- TestGeneratorLibrary/TestStructureInfo/ConstructorInfo.cs
+++ TestGeneratorLibrary/TestStructureInfo/ConstructorInfo.cs
@@ -5,9 +5,9 @@ namespace TestGeneratorLibrary.TestStructureInfo
     public class ConstructorInfo
     {
         public string Name { get; private set; }
-        public List<string> Parameters { get; private set; }
+        public List<ParameterInfo> Parameters { get; private set; }
 
-        public ConstructorInfo(List<string> parameters, string name)
+        public ConstructorInfo(List<ParameterInfo> parameters, string name)
         {
             this.Name = name;
             this.Parameters = parameters;
